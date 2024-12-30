cfd4bcd463c9d006105664bd041f8983d99cefce
Soumyadeep Chatterjee <soumyadeepchatterjee803@gmail.com>
2024-12-30 15:08:34 -0500

    built a demo list and delete functionality

---
+++ iExpense/iExpense/ContentView.swift
@@ -0,0 +1,53 @@
+//
+//  ContentView.swift
+//  iExpense
+//
+//  Created by Soumyadeep Chatterjee on 11/23/24.
+//
+
+import Observation
+import SwiftUI
+
+//Represents a single expense
+struct ExpenseItem {
+    let name: String
+    let type: String
+    let amount: Double
+}
+
+@Observable
+class Expenses {
+    //Creates an empty array of expense items
+    var items = [ExpenseItem]()
+}
+
+struct ContentView: View {
+    //Instance of your Expenses class
+    @State private var expenses = Expenses()
+    var body: some View {
+        NavigationStack {
+            List {
+                //"id: \.name" -> identify each expenses uniquely by it's name
+                ForEach(expenses.items, id: \.name) { item in
+                    Text(item.name)
+                }
+                .onDelete(perform: removeItems)
+            }
+            .navigationTitle("iExpense")
+            .toolbar {
+                Button("Add Expense", systemImage: "plus") {
+                    let expense = ExpenseItem(name: "Test", type: "Personal", amount: 5)
+                    expenses.items.append(expense)
+                }
+            }
+        }
+    }
+    
+    func removeItems(at offsets: IndexSet){
+        expenses.items.remove(atOffsets: offsets)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Soumyadeep Chatterjee on 12/30/24.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
